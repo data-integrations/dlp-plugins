ca35e972592af062a84b4fd2a3c1c4a4840a0ad7
Mo Eseifan <meseifan@google.com>
2019-12-20 12:16:14 -0500

    (CDAP-16195) Added more transforms to redaction plugin

---
+++ src/main/java/io/cdap/plugin/dlp/configs/RedactTransformConfig.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright © 2019 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package io.cdap.plugin.dlp.configs;
+
+import com.google.privacy.dlp.v2.PrimitiveTransformation;
+import com.google.privacy.dlp.v2.RedactConfig;
+import io.cdap.cdap.api.data.schema.Schema;
+import io.cdap.cdap.etl.api.FailureCollector;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Implementing the DlpTransformConfig interface for the DLP {@link RedactConfig}
+ */
+public class RedactTransformConfig implements DlpTransformConfig {
+
+  private final Schema.Type[] supportedTypes = new Schema.Type[]{Schema.Type.STRING};
+
+  @Override
+  public PrimitiveTransformation toPrimitiveTransform() {
+    return PrimitiveTransformation.newBuilder().setRedactConfig(RedactConfig.newBuilder().build()).build();
+  }
+
+  @Override
+  public void validate(FailureCollector collector, String widgetName, ErrorConfig errorConfig) {
+    return;
+  }
+
+  @Override
+  public List<Schema.Type> getSupportedTypes() {
+    return Arrays.asList(supportedTypes);
+  }
+}
